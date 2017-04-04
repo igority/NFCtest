68ea39b8c44f390346359abe0cf0f3a83866966f
igority <igority@gmail.com>
2017-04-05 01:49:26 +0200

    tutorial [2] finished

--- app/src/main/java/com/trex/igor/nfctest/MainActivity.java
+++ app/src/main/java/com/trex/igor/nfctest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.trex.igor.nfctest;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,17 +10,44 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    NfcAdapter nfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter != null && nfcAdapter.isEnabled()) {
-            Toast.makeText(this, "NFC is working! :)", Toast.LENGTH_SHORT).show();
+           // Toast.makeText(this, "NFC is working! :)", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "NFC not working :(", Toast.LENGTH_SHORT).show();
+            finish();
+           // Toast.makeText(this, "NFC not working :(", Toast.LENGTH_SHORT).show();
         }
     }
 
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Toast.makeText(this, "NFC intent received!", Toast.LENGTH_SHORT).show();
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this,pendingIntent,intentFilter,null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        nfcAdapter.disableForegroundDispatch(this);
+        super.onPause();
+    }
 }
