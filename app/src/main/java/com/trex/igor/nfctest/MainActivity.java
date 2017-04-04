9a901751a215b4a7117a8b50cd346db9fd7f76be
igority <igority@gmail.com>
2017-04-04 17:38:56 +0200

    tut 1 complete

--- app/src/main/java/com/trex/igor/nfctest/MainActivity.java
+++ app/src/main/java/com/trex/igor/nfctest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.trex.igor.nfctest;
 
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC is working! :)", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "NFC not working :(", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
